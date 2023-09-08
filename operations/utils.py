f2b8406956d9d808612fdaf5fe06f257abebd89b
Aleksey D <asdzog@mail.ru>
2023-09-08 05:23:27 +0400

    modify func read_last_history in utils.py

--- operations/utils.py
+++ operations/utils.py
@@ -2,7 +2,12 @@ import json
 DATA = "operations.json"
 
 
-def read_last_history():
+def read_last_history() -> list:
+    """
+    Reads file with operations history and returns last five ones of them
+    """
     with open(DATA, 'r', encoding='utf-8') as data:
         operations = json.loads(data.read())
-    return operations
+    sorted_operations = sorted(operations, key=lambda x: (x.get('state', 'unkwnown'), x.get('date')), reverse=True)
+    last_operations = [op for op in sorted_operations[:10] if op][:5]
+    return last_operations
