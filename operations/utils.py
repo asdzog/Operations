ef321b3ea3147b0ae5ae73f278eac4801ac337b5
Aleksey D <asdzog@mail.ru>
2023-09-08 06:54:29 +0400

    add func print_operation to utils.py

--- operations/utils.py
+++ operations/utils.py
@@ -11,3 +11,22 @@ def read_last_history() -> list:
     sorted_operations = sorted(operations, key=lambda x: (x.get('state', 'unkwnown'), x.get('date')), reverse=True)
     last_operations = [op for op in sorted_operations[:10] if op][:5]
     return last_operations
+
+
+def print_operation(operation: dict) -> str:
+    """
+    Returns operation information according to requirement format
+    """
+    op_date = '.'.join(operation['date'][:operation['date'].index('T')].split('-')[::-1])
+    descript = operation['description']
+    source = operation.get('from', 'Вкладчик')
+    if source != 'Вкладчик':
+        account_out = source.split()
+        acc_out_num = account_out[-1]
+        account_out[-1] = f'{acc_out_num[:4]} {acc_out_num[4:6]}** **** {acc_out_num[-4:]}'
+        source = ' '.join(account_out)
+    account_in, acc_in_num = operation['to'].split()
+    amount = operation['operationAmount']['amount']
+    currency = operation['operationAmount']['currency']['name']
+    operation = f'{op_date} {descript}\n{source} -> {account_in} **{acc_in_num[-4:]}\n{amount} {currency}'
+    return operation
