faa33a14fc22e72e83e6d0146bb61da75661d561
Aleksey D <asdzog@mail.ru>
2023-09-10 13:44:46 +0400

    split function to several smaller ones, add new functions

--- operations/utils.py
+++ operations/utils.py
@@ -1,32 +1,53 @@
 import json
-DATA = "operations.json"
 
 
-def read_last_history() -> list:
+def read_operations(filename) -> list:
     """
     Reads file with operations history and returns last five ones of them
     """
-    with open(DATA, 'r', encoding='utf-8') as data:
+    with open(filename, 'r', encoding='utf-8') as data:
         operations = json.loads(data.read())
-    sorted_operations = sorted(operations, key=lambda x: (x.get('state', 'unkwnown'), x.get('date')), reverse=True)
-    last_operations = [op for op in sorted_operations[:10] if op][:5]
+    return operations
+
+
+def sort_history(operations: list, operations_amount: int) -> list:
+    """
+    Sorts list of operations by date and status 'EXECUTED' and returns list of needed amount operations
+    """
+    sorted_operations = sorted(operations, key=lambda x: (x.get('state', 'unknown'), x.get('date')), reverse=True)
+    last_operations = [op for op in sorted_operations[:operations_amount * 2] if op][:operations_amount]
     return last_operations
 
 
-def print_operation(operation: dict) -> str:
+def encode_in_account(operation: dict) -> str:
     """
-    Returns operation information according to requirement format
+    Encodes source account number into a required format
     """
-    op_date = '.'.join(operation['date'][:operation['date'].index('T')].split('-')[::-1])
-    descript = operation['description']
+    account_in, acc_in_num = operation['to'].split()
+    return f'{account_in} **{acc_in_num[-4:]}'
+
+
+def encode_out_account(operation: dict) -> str:
+    """
+    Encodes purpose account number into a required format
+    """
     source = operation.get('from', 'Вкладчик')
     if source != 'Вкладчик':
         account_out = source.split()
         acc_out_num = account_out[-1]
         account_out[-1] = f'{acc_out_num[:4]} {acc_out_num[4:6]}** **** {acc_out_num[-4:]}'
         source = ' '.join(account_out)
-    account_in, acc_in_num = operation['to'].split()
+    return source
+
+
+def print_operation(operation: dict) -> str:
+    """
+    Returns operation information according to output required format
+    """
+    oper_date = '.'.join(operation['date'][:operation['date'].index('T')].split('-')[::-1])
+    description = operation['description']
     amount = operation['operationAmount']['amount']
     currency = operation['operationAmount']['currency']['name']
-    operation = f'{op_date} {descript}\n{source} -> {account_in} **{acc_in_num[-4:]}\n{amount} {currency}'
-    return operation
+    source = encode_out_account(operation)
+    purpose = encode_in_account(operation)
+    return f'{oper_date} {description}\n{source} -> {purpose}\n{amount} {currency}'
